00de682082c415b37e77e075cb0a6142c78f9747
Igor Wiedler <igor@wiedler.ch>
2009-09-20 18:52:57 +0200

    moving tree and forrest to their own models

--- src/controller/Kruskal.java
+++ src/controller/Kruskal.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashSet;
 
 import model.Edge;
+import model.Forrest;
 import model.Graph;
 import model.Vertex;
 import tools.Shell;
@@ -105,8 +106,6 @@ public class Kruskal {
 		try {
 			// make an animated gif from out images
 			Shell.exec("gifsicle --delay=200 --loop kruskal*.gif > anim_kruskal.gif");
-			
-			// clean up
 			Shell.exec("rm kruskal*.gif");
 		} catch (IOException ex) {
 			ex.printStackTrace();
@@ -134,153 +133,6 @@ public class Kruskal {
 			}
 		}
 		return shortest;
-	}
-	
-	// a tree contains a set of edges
-	public class Tree {
-		private HashSet<Edge> edges = new HashSet<Edge>();
-		
-		public void add(Edge e) {
-			edges.add(e);
-		}
-		
-		public void remove(Edge e) {
-			edges.remove(e);
-		}
-		
-		public HashSet<Edge> getEdges() {
-			return edges;
-		}
-		
-		// contains edge or reverse-edge
-		public boolean contains(Edge edge) {
-			return edges.contains(edge) || edges.contains(edge.getTarget().findEdge(edge.getOrigin()));
-		}
-		
-		// does the edge touch the tree
-		public boolean connects(Edge edge) {
-			for (Edge e : edges) {
-				if (edge.getOrigin() == e.getOrigin() || edge.getOrigin() == e.getTarget()) return true;
-				if (edge.getTarget() == e.getOrigin() || edge.getTarget() == e.getTarget()) return true;
-			}
-			return false;
-		}
-		
-		// do both edges touch the tree
-		public boolean connectsBoth(Edge edge) {
-			int c = 0;
-			for (Edge e : edges) {
-				if (edge.getOrigin() == e.getOrigin() || edge.getOrigin() == e.getTarget()) c++;
-				if (edge.getTarget() == e.getOrigin() || edge.getTarget() == e.getTarget()) c++;
-				
-				if (c >= 2) {
-					// both edges touch the tree
-					return true;
-				}
-			}
-			return false;
-		}
-		
-		// number of edges
-		public int size() {
-			return edges.size();
-		}
-	}
-	
-	// a forrest contains a list of separate (disconnected) trees
-	public class Forrest {
-		private HashSet<Tree> trees = new HashSet<Tree>();
-		
-		public void add(Tree t) {
-			trees.add(t);
-		}
-		
-		public void remove(Tree t) {
-			trees.remove(t);
-		}
-		
-		// merge two trees
-		public void merge(Tree t1, Tree t2) {
-			for (Edge e : t1.getEdges()) {
-				t2.add(e);
-			}
-			
-			// remove t1
-			remove(t1);
-			
-			// update reference
-			t1 = t2;
-		}
-		
-		public int size() {
-			return trees.size();
-		}
-		
-		public HashSet<Tree> getTrees() {
-			return trees;
-		}
-		
-		// add edge to forrest
-		public void add(Edge e) {
-			Tree tree1 = null;
-			Tree tree2 = null;
-			for (Tree t : trees) {
-				if (t.connects(e)) {
-					tree1 = t;
-					break;
-				}
-			}
-			if (tree1 != null) {
-				for (Tree t : trees) {
-					if (t != tree1 && t.connects(e)) {
-						tree2 = t;
-						break;
-					}
-				}
-			}
-			
-			if (tree1 != null && tree2 != null) {
-				// if edge connects to two trees, merge
-				tree1.add(e);
-				tree2.add(e);
-				merge(tree1, tree2);
-			} else if (tree1 != null) {
-				// connects to one tree, add
-				tree1.add(e);
-			} else {
-				// new tree, create
-				Tree t = new Tree();
-				t.add(e);
-				add(t);
-			}
-		}
-		
-		// contains edge
-		public boolean contains(Edge e) {
-			for (Tree t : trees) {
-				if (t.contains(e)) return true;
-			}
-			return false;
-		}
-		
-		// does edge connect to tree
-		public boolean connectsBoth(Edge e) {
-			for (Tree t : trees) {
-				if (t.connectsBoth(e)) {
-					return true;
-				}
-			}
-			return false;
-		}
-		
-		// count all edges
-		public int countEdges() {
-			int count = 0;
-			for (Tree t : trees) {
-				count += t.size();
-			}
-			return count;
-		}
 	}
 	
 	public static void main(String[] args) {
