3d02de0704d92372b1a7e44d4029b49fe853b946
Igor Wiedler <igor@wiedler.ch>
2009-12-10 22:53:41 +0100

    enable anti-aliasing on the canvas

--- src/view/GraphCanvas.java
+++ src/view/GraphCanvas.java
@@ -4,6 +4,8 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -68,6 +70,10 @@ public class GraphCanvas extends Canvas {
 	// (re)paint the graph
 	public void paint(Graphics g) {
 		super.paint(g);
+		
+		// enable anti-aliasing
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		// draw edges
 		for (Edge e : graph.getEdges()) {
