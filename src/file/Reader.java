d65ac1c3c8ee77a8a03775582b776e1e2e8e5d57
Igor Wiedler <igor@wiedler.ch>
2009-12-17 15:57:49 +0100

    boldi reader/writer documentation

--- src/file/Reader.java
+++ src/file/Reader.java
@@ -9,28 +9,36 @@ import model.Graph;
 import model.Vertex;
 
 public class Reader {
+	
 	HashMap<String, Vertex> temp = new HashMap<String, Vertex>();
 	Graph g = new Graph();
 
 	public Graph getGraph(File f){
 		try{
 			FileReader fr = new FileReader(f);
+			// neuer Bufferreader
 			BufferedReader input = new BufferedReader(fr);
 			
 			String inputLine = "";
+			
+			// solange noch Linien vorhanden sind
 			while((inputLine = input.readLine())!=null){
 			
+			// Splitet die Linien mit den Tablulatoren
 			String[] tokens = inputLine.split("\t");
 			
 			
-			
+			// Wenn der erste Character "v" ist, wird ein neuer Vertex mit den nŠchsten drei Character erzeugt.
 			if(tokens[0].equals("v")){
 					Vertex v = new Vertex(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
 					temp.put(v.getName(), v);
+					//Vertex wird zum Graph g hinzugefügt
 					g.add(v);
 			}
-				
+			
+			// Wenn der erste Character "e" ist, wird eine neue Edge mit den nŠchsten drei Character erzeugt.
 			if(tokens[0].equals("e")){
+					// hier werden im Graph g zwei Vertex verbunden
 					g.connect(temp.get(tokens[1]), temp.get(tokens[2]), Integer.parseInt(tokens[3]));
 				}		
 			}
@@ -40,6 +48,7 @@ public class Reader {
 		{
 			e.printStackTrace();
 		}
+		//gibt den Graph g zurŸck
 		return g;
 		
 	}
--- src/file/Writer.java
+++ src/file/Writer.java
@@ -10,21 +10,26 @@ import model.Vertex;
 
 public class Writer {
 	
+	// für die Funktion Writer m¸ssen der Graph g und den gew¸nschte Namen für das Text File ¸bergeben werden
 	public Writer(Graph g, File f) throws IOException{
+		// Das File mit dem gewünschten Namen wird erzeugt
 		FileWriter file = new FileWriter(f);
+		// Solange Vertices im Graph g vorhanden sind
 		for(Vertex v : g.getVertices()){
-			file.write("v\t");
-			file.write(v.getName()+"\t");
-			file.write(v.getX()+"\t");
-			file.write(v.getY()+"\n");
+			file.write("v\t");	// gibt die Identifikation für Vertex
+			file.write(v.getName()+"\t");	// der Name für den Vertex
+			file.write(v.getX()+"\t");	// die X-Koordinate für den Vertex
+			file.write(v.getY()+"\n");	// die Y-Koordinate für den Vertex
 		}
+		// Solange Edges in im Graph vorhandne sind
 		for(Edge e : g.getEdges()){
-			file.write("e\t");
-			file.write(e.getV1()+"\t");
-			file.write(e.getV2()+"\t");
-			file.write(e.getWeight()+"\n");
+			file.write("e\t");	// Identifikation für Edge
+			file.write(e.getV1()+"\t");	// Start Vertex der Edge
+			file.write(e.getV2()+"\t");	// End Vertex der Edge
+			file.write(e.getWeight()+"\n");	// Gewichtung der Edge
 		}
 		
+		// file wird geschlossen
 		file.close();
 	}
 
