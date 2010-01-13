c22167ab504840ba95dd772900db9da2060f51ed
Igor Wiedler <igor@wiedler.ch>
2010-01-13 21:08:23 +0100

    javadoc for writerfilefilter

--- src/misc/WriterFileFilter.java
+++ src/misc/WriterFileFilter.java
@@ -6,22 +6,47 @@ import javax.swing.filechooser.FileFilter;
 
 import file.GraphWriter;
 
+/**
+ * FileFilter containing a GraphWriter
+ */
 public class WriterFileFilter extends FileFilter {
-	
+	/**
+	 * GraphWriter providing a description
+	 */
 	private GraphWriter w;
 	
+	/**
+	 * default constructor
+	 * 
+	 * @param w writer
+	 */
 	public WriterFileFilter(GraphWriter w) {
 		this.w = w;
 	}
 	
+	/**
+	 * accept all files
+	 * 
+	 * @param f file to be accepted
+	 */
 	public boolean accept(File f) {
 		return true;
 	}
-
+	
+	/**
+	 * file filter description
+	 * 
+	 * @return writer name
+	 */
 	public String getDescription() {
 		return w.getName();
 	}
 	
+	/**
+	 * getter for writer
+	 * 
+	 * @return writer
+	 */
 	public GraphWriter getWriter() {
 		return w;
 	}
